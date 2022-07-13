032d23ce518f983243c1b4356d912176cda22898
Ronaldo Sobrinho <rpvsobrinho@gmail.com>
2022-07-13 15:54:13 -0300

    Estoque

---
+++ Ebook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RonaldoSobrinho
+{
+    [System.Serializable]
+    internal class Ebook : Produto, IEstoque
+    {
+        public string autor;
+        private int vendas;
+
+        public Ebook(string nome, float preco, string autor)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.autor = autor;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine("Não é possivel dar entrada, produto digital");
+            Console.ReadLine();
+
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar venda no E-book {nome}");
+            Console.WriteLine("Digite a quantidade que você quer dar entrada: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vendas = vendas + entrada;
+            Console.WriteLine("Saida registrada");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Vendas: {vendas}");
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
---
+++ IEstoque.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RonaldoSobrinho
+{
+    internal interface IEstoque
+    {
+        void Exibir();
+        void AdicionarEntrada();
+        void AdicionarSaida();
+
+    }
+}
